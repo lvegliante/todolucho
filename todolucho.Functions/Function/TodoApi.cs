ca24d79decf3477a7d3c386f31ba36e186660c6e
LUIS ALFREDO VEGLIANTE PALMERA <luisvegliante147779@correo.itm.edu.co>
2021-08-19 20:05:42 -0500

    Schedule function

--- todolucho.Functions/Function/TodoApi.cs
+++ todolucho.Functions/Function/TodoApi.cs
@@ -126,6 +126,7 @@ namespace todolucho.Functions.Function
                 Result = todos
             });
         }
+
         [FunctionName(nameof(GetTodoById))]
         public static IActionResult GetTodoById(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "todo/{id}")] HttpRequest req,
@@ -145,6 +146,37 @@ namespace todolucho.Functions.Function
             }
 
             string message = $"Todo: {id} retrieved";
+            log.LogInformation(message);
+
+            return new OkObjectResult(new Response
+            {
+                IsSuccess = true,
+                Message = message,
+                Result = todoEntity
+            });
+        }
+
+        [FunctionName(nameof(DeleteTodo))]
+        public static async Task<IActionResult>DeleteTodo(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "todo/{id}")] HttpRequest req,
+            [Table("todo", "TODO", "{id}", Connection = "AzureWebJobsStorage")] TodoEntity todoEntity,
+            [Table("todo", Connection = "AzureWebJobsStorage")] CloudTable todoTable,
+            string id,
+            ILogger log)
+        {
+            log.LogInformation($"Delete todo: {id}, recived.");
+
+            if (todoEntity == null)
+            {
+                return new BadRequestObjectResult(new Response
+                {
+                    IsSuccess = false,
+                    Message = "Todo not found."
+                });
+            }
+
+            await todoTable.ExecuteAsync(TableOperation.Delete(todoEntity));
+            string message = $"Todo: {todoEntity.RowKey}, deleted.";
             log.LogInformation(message);
 
             return new OkObjectResult(new Response
