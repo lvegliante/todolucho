64c5bfdac6b613e51528024fd8358ec1acf4cf54
LUIS ALFREDO VEGLIANTE PALMERA <luisvegliante147779@correo.itm.edu.co>
2021-08-21 21:43:18 -0500

    Pruebas Unitarias Listas

---
+++ todolucho.Tests/Tests/ScheduledFunctionTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using todolucho.Functions.Function;
+using todolucho.Tests.Helpers;
+using Xunit;
+
+namespace todolucho.Tests.Tests
+{
+    public class ScheduledFunctionTest
+    {
+        [Fact]
+        public void ScheduledFunction_Should_Log_Message()
+        {
+            //Arrange
+            MockCloudTableTodos mockTodos = new MockCloudTableTodos(new Uri("http://127.0.0.1:10002/devstoreaccount1/reports"));
+           ListLogger logger = (ListLogger)TestFactory.CreateLogger(LoggerTypes.List);
+            //Act
+            ScheduleFunction.Run(null, mockTodos, logger);
+            string message = logger.Logs[0];
+            //Assert
+            Assert.Contains("Deleting completed", message);
+        }
+    }
+}
---
+++ todolucho.Tests/Tests/TodoApiTest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using todolucho.Common.Models;
+using todolucho.Functions.Function;
+using todolucho.Tests.Helpers;
+using Xunit;
+
+namespace todolucho.Tests.Tests
+{
+    public class TodoApiTest
+    {
+        private readonly ILogger logger = TestFactory.CreateLogger();
+        [Fact]
+        public async void CreateTodo_Should_Return_200()
+        {
+            //Arrenge
+            MockCloudTableTodos mockTodos = new MockCloudTableTodos(new Uri("http://127.0.0.1:10002/devstoreaccount1/reports"));
+            Todo todorequest = TestFactory.GetTodoRequest();
+            DefaultHttpRequest request = TestFactory.CreateHttpRequest(todorequest);
+            //Act
+            IActionResult response = await TodoApi.CreateTodo(request, mockTodos, logger);
+            //Assert
+            OkObjectResult result = (OkObjectResult)response;
+            Assert.Equal(StatusCodes.Status200OK, result.StatusCode);
+
+        }
+        [Fact]
+        public async void UpdateTodo_Should_Return_200()
+        {
+            //Arrenge
+            MockCloudTableTodos mockTodos = new MockCloudTableTodos(new Uri("http://127.0.0.1:10002/devstoreaccount1/reports"));
+            Todo todorequest = TestFactory.GetTodoRequest();
+            Guid todoId = Guid.NewGuid();
+            DefaultHttpRequest request = TestFactory.CreateHttpRequest(todoId,todorequest);
+            //Act
+            IActionResult response = await TodoApi.UpdateTodo(request, mockTodos, todoId.ToString(), logger);
+            //Assert
+            OkObjectResult result = (OkObjectResult)response;
+            Assert.Equal(StatusCodes.Status200OK, result.StatusCode);
+
+        }
+    }
+}
